9626fc1fb0d5e568ee3b0ff136fb98d8f3779c5d
ChristianMarcel-bob <christian.m.soares@aluno.senai.br>
2026-09-14 15:30:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula inicial teste</title>
+</head>
+<body>
+    <?php
 echo "<h1>Aula inicial desenvolvimento de sistemas</h1>";
+
+?>
+
+    
+</body>
+</html>
