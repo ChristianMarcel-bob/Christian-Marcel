04ab04bc3ebd0dd13e2fa7e53a5758e7c2f31baa
ChristianMarcel-bob <christian.m.soares@aluno.senai.br>
2026-09-14 16:33:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,6 @@
 $nome = "Christian";
 $idade = "37";
 
-<h1>nome: <?= $nome ?></h1>;
-<p> idade: <?= $idade ?> </p>;
-    if ($idade >= 18) {
-        $status = "Maior de idade";
-    } else {
-        $status = "Menor de idade";
-    }
-
 
 ?>
 
