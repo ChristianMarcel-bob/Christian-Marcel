4527c5e38604ddab7c5c5d49e4eabeb2744145b0
ChristianMarcel-bob <christian.m.soares@aluno.senai.br>
2026-09-14 16:19:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,16 @@
 <body>
     <?php
 echo "<h1>Aula inicial</h1>";
+$nome="Christian";
+echo "<h1>olá, $ Christian</h1>";
+$nome = "Christian";
+$idade = "37";
 
 ?>
+
+<h1>nome: <?=$christian  ?></h1>;
+<p> idade: 37 </p>;
+
 
     
 </body>
