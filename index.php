301834a40455f302fe6332d68bf4ea1bd93639e4
ChristianMarcel-bob <christian.m.soares@aluno.senai.br>
2026-09-15 16:01:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,69 +1,45 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula inicial teste</title>
-</head>
-<body>
-    <?php
-    $nome = "Christian";
-    $idade = 37;
-
-
+<?php
     $nome="";
     $idade=0;
     $resultado="";
+
     if ($_SERVER["REQUEST_METHOD"] =="POST"){
         $nome = $_POST["nome"];
         $idade = $_POST["idade"];
 
+        if ($idade >= 18) {
+            $status = "Maior de idade";
+        } else {
+            $status = "Menor de idade";
+        }
+    } //O IF ESTÁ PERGUNTANDO SE O FORMULÁRIO TEM MÉTODO DE POST
+?>
 
-    if ($idade >= 18) {
-        $status = "Maior de idade";
-    } else {
-        $status = "Menor de idade";
-    }
-
-
-
- } //O IF ESTÁ PERGUNTANDO SE O FORMULÁRIO TEM MÉTODO DE POST
-
-
-
-    ?>
-  
-    <h1>Nome: <?= $nome ?></h1>
-    <p>Idade: <?= $idade ?></p>
-    <p>Status: <?= $status ?></p>
-
- 
-    <h2>atualizar cadastro</h2>
-    <form method="POST">
-
-        <div>
-            <label for="nome">christian marcel:</label>
-            <input type="text" id="nome" name="nome" value="<?= $nome?>">
-
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aula inicial teste</title>
+    </head>
+    <body>
+        <h2>atualizar cadastro</h2>
+        <form method="POST">
+            <div>
+                <label for="nome">christian marcel:</label>
+                <input type="text" id="nome" name="nome" value="<?= $nome?>">
             </div>
-        
-        <div>
-            <label for="idade">alterar idade:</label>
-            <input type="number" id="idade" name="idade" value="<?= $idade ?>">
-        </div>
-          
-        <button type="submit">Atualizar</button>
+            <div>
+                <label for="idade">alterar idade:</label>
+                <input type="number" id="idade" name="idade" value="<?= $idade ?>">
+            </div>
+            <button type="submit">Atualizar</button>
+        </form>
+        <?php if ($resultado != "") { ?>
+            <p><?= $resultado ?></p>
+        <?php } ?>
 
-    </form>
-
-
-    <?php if ($resultado != "") { ?>
-        
-
-
-    <?php } ?>
-
-</body>
+    </body>
 </html>
 
 
